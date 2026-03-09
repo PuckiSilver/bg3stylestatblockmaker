e4208552cb846a12dd9b91967615fbc4b957bcc5
PuckiSilver <pucki.silver@gmail.com>
2026-03-09 13:22:29 +0100

    start adding importing

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import EditStatblock from './components/EditStatblock.vue';
 import yaml from 'js-yaml';
 
 const message = ref('');
+const eToolsImport = ref('');
 
 const statblockData = computed(old => {
   try {
@@ -19,12 +20,50 @@ const statblockData = computed(old => {
 })
 
 const a = ref({image_modifications:{},stats:[10,10,10,10,10,10]})
+
+const eToolsSizeLookupTable = {
+  'M': 'Medium',
+  'L': 'Large',
+}
+
+const import5eTools = () => {
+  const i = JSON.parse(eToolsImport.value)
+  a.value = {
+    name: i.name,
+    size: eToolsSizeLookupTable[i.size],
+    type: i.type,
+    ac: i.ac?.reduce((a, b) => a + (b.ac ?? b), 0),
+    hp: `${i.hp.average} ${i.hp.formula}`,
+    cr: i.cr,
+    senses: [
+      i.passive ? `Passive Perception ${i.passive}` : undefined,
+      ...(i.senses ? i.senses : [])
+    ].filter(e => e).reduce((a, b) => a + ', ' + b),
+    languages: i.languages?.reduce((a, b) => a + ', ' + b),
+    speed: [
+      i.speed?.walk ? `Walking Speed: ${i.speed.walk}ft.` : undefined,
+      i.speed?.swim ? `Swimming Speed: ${i.speed.swim}ft.` : undefined,
+      i.speed?.fly ? `Flying Speed: ${i.speed.fly}ft.` : undefined,
+      i.speed?.burrow ? `Burrowing Speed: ${i.speed.burrow}ft.` : undefined,
+    ].filter(e => e).reduce((a, b) => a + ', ' + b),
+    stats:[i.str,i.dex,i.con,i.int,i.wis,i.cha],
+    image: i.hasToken
+    ? `https://5e.tools/img/bestiary/tokens/${i.source}/${encodeURI(i.name)}.webp`
+    : undefined,
+    image_modifications:{},
+    actions: i.action?.map(e => ({name: e.name, desc: e.entries.reduce((a, b) => a + '\n\n' + b)})),
+    bonus_actions: i.bonus?.map(e => ({name: e.name, desc: e.entries.reduce((a, b) => a + '\n\n' + b)})),
+    traits: i.trait?.map(e => ({name: e.name, desc: e.entries.reduce((a, b) => a + '\n\n' + b)})),
+  }
+}
 </script>
 
 <template>
   <main>
     <h1>BG3 Style Stat Block Maker</h1>
     <textarea v-model="message" placeholder="Paste a yml statblock here in order to display it..." rows="5" cols="65"></textarea>
+    <textarea v-model="eToolsImport" />
+    <button @click="import5eTools">Import from 5eTools</button>
     {{ a }}
     <div class="row">
       <EditStatblock v-model="a"/>
